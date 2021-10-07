a16039e1b108476d512ed41d6e1d54e3d44edbd8
Kristina <kristina.stepanaviciene@mygom.tech>
2021-10-07 10:43:36 +0300

    chore: cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
 import * as Sentry from '@sentry/react';
-import i18n from 'i18next';
 
 import ErrorPage from './containers/Error/ErrorPage';
 import AppRoutes from './containers/Routes/AppRoutes';
@@ -17,10 +16,6 @@ const App: React.FC = () => {
     <Sentry.ErrorBoundary fallback={<ErrorPage message={t('internal.server.error')} />}>
       <ErrorBoundary>
         <BrowserRouter>
-          <button type="button" onClick={() => i18n.changeLanguage('en-GB')}>
-            Change language
-          </button>
-          <hr />
           <AppRoutes />
         </BrowserRouter>
       </ErrorBoundary>
