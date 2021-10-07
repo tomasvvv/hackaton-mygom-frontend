4080a2c173f2061c81e3b3fa33f970306f2849fa
Vitalijus Zimaitis <vz@frontit.dk>
2021-10-07 10:41:19 +0300

    Cleanup

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,19 +1,14 @@
-import * as React from 'react';
+import React from 'react';
 
 import { TRouteType } from './containers/Routes/RouteManager';
 
 const Home = React.lazy(() => import(/* webpackChunkName: "HomePage" */ './pages/Home'));
-const Forms = React.lazy(() => import(/* webpackChunkName: "Forms" */ './pages/Forms'));
-const Users = React.lazy(() => import(/* webpackChunkName: "Users" */ './pages/Users'));
 const NotFound = React.lazy(
   () => import(/* webpackChunkName: "NotFound" */ './containers/Error/NotFound'),
 );
 
 export const routes = {
   home: '/',
-  userList: '/users',
-  userProfile: '/user/:id',
-  forms: '/forms',
 };
 
 export const Routes: TRouteType = {
@@ -21,27 +16,6 @@ export const Routes: TRouteType = {
     path: routes.home,
     exact: true,
     component: Home,
-  },
-  forms: {
-    path: routes.forms,
-    exact: true,
-    component: Forms,
-  },
-  userList: {
-    path: routes.userList,
-    exact: true,
-    component: Users,
-  },
-  test: {
-    path: '/test',
-    exact: true,
-    redirect: () => '/',
-  },
-  authorized: {
-    path: '/authorized',
-    authorized: true,
-    exact: true,
-    component: Users,
   },
   notFound: {
     path: '*',
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 
-import { routes } from '../Routes';
-
-const Home: React.FC = (): JSX.Element => {
-  const { t } = useTranslation();
-
-  return (
-    <>
-      <h1>
-        {t('Welcome to React', {
-          color: 'red',
-          interpolation: { escapeValue: false },
-        })}
-      </h1>
-      <Link to={routes.userList}>Users</Link>
-      <br />
-      <Link to={routes.forms}>Formik</Link>
-    </>
-  );
-};
+const Home: React.FC = (): JSX.Element => (
+  <>
+    App
+  </>
+);
 
 export default Home;
