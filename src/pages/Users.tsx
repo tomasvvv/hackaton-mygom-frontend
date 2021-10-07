34691a998c84f85eed4215bb8a638314d7f6745d
Kristina <kristina.stepanaviciene@mygom.tech>
2021-10-07 17:38:31 +0300

    feat: add user modal

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -3,24 +3,54 @@ import {
   Box,
   Button,
   Container,
+  Modal,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  TextField,
+  Typography,
 } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 import { useAddUserMutation, useGetUsersQuery } from '../services/UserService';
 
+const useUserStyles = makeStyles(() => ({
+  modal: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '30rem',
+    background: 'white',
+    border: '2px solid #000',
+    padding: '1rem',
+  },
+  fields: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  addButton: {
+    marginTop: '2rem',
+  },
+  form: {
+    textAlign: 'center',
+  },
+}));
 const Users = React.memo(() => {
+  const classes = useUserStyles();
   const { data } = useGetUsersQuery();
   // eslint-disable-next-line no-unused-vars
   const [addUser] = useAddUserMutation();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <Container>
       <Box mt={5}>
-        <Button variant="contained" type="submit" color="primary">
+        <Button variant="contained" type="button" color="primary" onClick={handleOpen}>
           Add User
         </Button>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -40,7 +70,30 @@ const Users = React.memo(() => {
           </TableBody>
         </Table>
       </Box>
-
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className={classes.modal}>
+          <Typography variant="h5" mb="1rem">Add user</Typography>
+          <form className={classes.form}>
+            <div className={classes.fields}>
+              <TextField required id="outlined-required" label="Name" />
+              <TextField required id="outlined-required" label="Role" />
+            </div>
+            <Button
+              variant="contained"
+              type="submit"
+              color="primary"
+              className={classes.addButton}
+            >
+              Add
+            </Button>
+          </form>
+        </div>
+      </Modal>
     </Container>
   );
 });
